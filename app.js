cb9ee43cdb2d6af1638704e31e2a86adfc070a71
Sami Salkosuo <sami.salkosuo@ieee.org>
2017-10-10 08:03:40 +0300

    added resize uploaded image and code cleaning

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*eslint-env node*/
 
 //------------------------------------------------------------------------------
-// node.js starter application for Bluemix
+// node.js  application 
 //------------------------------------------------------------------------------
 
 // This application uses express as its web server
@@ -17,6 +17,7 @@ var request = require('request');
 var async = require("async");
 var spawn = require('child_process').spawn;
 var spawnSync = require('child_process').spawnSync;
+var jimp = require('jimp');
 
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 
@@ -84,6 +85,7 @@ app.post('/file-upload', function(req, res) {
 
     var uploadDir = rootDir + "/" + id;
     var imagePath = uploadDir + "/image.jpg";
+    var imageResizedPath = uploadDir + "/image_resized.jpg";
     var jsonPath = uploadDir + "/image.json";
 
     if (!fs.existsSync(rootDir)) {
@@ -120,103 +122,59 @@ app.post('/file-upload', function(req, res) {
             res.status(500).send(err);
         } else {
             res.send('File uploaded!');
-            //update(sessionId, "file uploaded and saved to " + imagePath)
-            update(sessionId, "file uploaded")
-            generateImageTiles(sessionId, {
-                rootDir: rootDir,
-                id: id,
-                imagePath: imagePath,
-                imageDir: uploadDir,
-                tileWidth: tileWidth,
-                tileHeight: tileHeight
-            }, function(err, imageData) {
-                if (err) {
-                    update(sessionId, "parsing error: " + err.toString())
-                } else {
-                    //update(sessionId, "parsing complete")
-                    var imageData = imageData;
-                    imageData.imagePath = imagePath;
-                    processImages(sessionId, imageData, function(updatedImageData) {
-                        update(sessionId, "analysis complete")
-
-
-                        var json = JSON.stringify(updatedImageData);
-
-                        fs.writeFile(jsonPath, json, function(err) {
-                            if (err) return update(sessionId, err);
-                            //update(sessionId, 'wrote json data');
-
-                            var result = {
-                                imagePath: imagePath,
-                                jsonPath: jsonPath
+            jimp.read(imagePath).then(function (image) {
+                //resize image
+                image.resize(640, jimp.AUTO).write(imagePath,function(err,image){    
+                        //update(sessionId, "file uploaded and saved to " + imagePath)
+                        update(sessionId, "file uploaded")
+                        generateImageTiles(sessionId, {
+                            rootDir: rootDir,
+                            id: id,
+                            imagePath: imagePath,
+                            imageDir: uploadDir,
+                            tileWidth: tileWidth,
+                            tileHeight: tileHeight
+                        }, function(err, imageData) {
+                            if (err) {
+                                update(sessionId, "parsing error: " + err.toString())
+                            } else {
+                                //update(sessionId, "parsing complete")
+                                var imageData = imageData;
+                                imageData.imagePath = imagePath;
+                                processImages(sessionId, imageData, function(updatedImageData) {
+                                    update(sessionId, "analysis complete")
+            
+            
+                                    var json = JSON.stringify(updatedImageData);
+            
+                                    fs.writeFile(jsonPath, json, function(err) {
+                                        if (err) return update(sessionId, err);
+                                        //update(sessionId, 'wrote json data');
+            
+                                        var result = {
+                                            imagePath: imagePath,
+                                            jsonPath: jsonPath
+                                        }
+                                        dispatch(sessionId, "processingComplete", JSON.stringify(result))
+                                    });
+            
+                                });
                             }
-                            dispatch(sessionId, "processingComplete", JSON.stringify(result))
-                        });
 
-                    })
-                }
-            })
+                        })
+                });
+            }).catch(function (err) {
+
+                // handle an exception 
+                console.log(err);
+            });
         }
     });
 });
 
-function getPosition(row,col)
-{
-    var positionDictionary = {};
-    positionDictionary["11_1"]=[27,630];
-    positionDictionary["10_1"]=[57,515];
-    positionDictionary["9_1"]=[67,425];
-    positionDictionary["8_1"]=[105,345];
-    positionDictionary["7_1"]=[118,278];
-    positionDictionary["6_1"]=[131,216];
-    positionDictionary["5_1"]=[151,165];
-    positionDictionary["4_1"]=[163,121];
-    positionDictionary["3_1"]=[185,77];
-    positionDictionary["2_1"]=[194,42];
-    positionDictionary["1_1"]=[217,0];
-
-    positionDictionary["11_2"]=[826,704];
-    positionDictionary["10_2"]=[816,589];
-    positionDictionary["9_2"]=[733,422];
-    positionDictionary["8_2"]=[712,338];
-    positionDictionary["7_2"]=[699,266];
-    positionDictionary["6_2"]=[676,209];
-    positionDictionary["5_2"]=[642,156];
-    positionDictionary["4_2"]=[597,106];
-    positionDictionary["3_2"]=[586,67];
-    positionDictionary["2_2"]=[568,28];
-    positionDictionary["1_2"]=[550,0];
-    //console.log(positionDictionary);
-    return positionDictionary[row+"_"+col];
-}
-
-
-function  execChildProcessSync(image,tilesDir,row,col) {
-    var position=getPosition(row,col);
-    //console.log("position: "+position);
-    var location="+"+position[0]+"+"+position[1];
-    var childProcess1 = spawnSync("convert", [
-                image,
-                "-crop",
-                "150x40"+location,
-                tilesDir+"/tile_"+col+"_"+row+".jpg"
-            ]);
-    
-    if(childProcess1.status != 0)
-    {
-        console.log("Executed process for location "+location+"; row "+row+"; col "+col);
-        console.log("  Process pid: ",childProcess1.pid);
-        console.log("  Process status: ",childProcess1.status);
-        console.log("  Process signal: ",childProcess1.signal);
-        console.log("  Process stdout: ",childProcess1.stdout.toString('utf8'));
-        console.log("  Process stderr: ",childProcess1.stderr.toString('utf8'));
-        console.log("  Process error: ",childProcess1.error);
-    }
-}
-
 
 function generateImageTiles(sessionId, options, callback) {
-
+    //TODO: refactor this function away
     var imageSize = {};
     var parseData = {};
     parseData.tiles = [];
@@ -227,150 +185,8 @@ function generateImageTiles(sessionId, options, callback) {
     var childProcessError = undefined;
     
     callback(childProcessError, parseData);
-  /*  
-    //var fileName = parseFileName(options.imagePath);
-    var tilesDir = options.imagePath + "_tiles";
-    var tileWidth = options.tileWidth;
-    var tileHeight = options.tileHeight;
-
-    if (!fs.existsSync(tilesDir)) {
-        fs.mkdirSync(tilesDir);
-    }
-
-    var image = gm(options.imagePath)
-        .size(function(err, size) {
-
-            if (err) {
-                callback(err);
-                return;
-            }
-
-
-            imageSize = size;
-            //modify for customer POC
-            //var cols = Math.ceil(imageSize.width / tileWidth);
-            //var rows = Math.ceil(imageSize.height / tileHeight);
-            var cols = 2 // 2 "cols"
-            var rows = 11 // 11 "rows"
-
-            parseData.imageWidth = size.width;
-            parseData.imageHeight = size.height;
-            parseData.dimensions = {
-                cols: cols,
-                rows: rows
-            }
-
-            parseData.tiles = [];
-            
-            //var command =  options.imagePath + ' -write mpr:XY +delete -respect-parentheses ( mpr:XY -crop 150x40+27+630 +repage +write "'+tilesDir+'/tile_1_1.jpg" ) ';
-            
-            update(sessionId, "Invoke: converting images... ");
-            console.log(sessionId, "Invoke: converting images... ");
-            console.log(sessionId, " ");
-            //console.log(sessionId, "Splitted args: " + command.split(" "));
-            //var childProcess = spawn("convert", command.split(" "));
-            //rows start from bottom left
-            execChildProcessSync(options.imagePath,tilesDir,11,1)
-            execChildProcessSync(options.imagePath,tilesDir,10,1)
-            execChildProcessSync(options.imagePath,tilesDir,9,1)
-            execChildProcessSync(options.imagePath,tilesDir,8,1)
-            execChildProcessSync(options.imagePath,tilesDir,7,1)
-            execChildProcessSync(options.imagePath,tilesDir,6,1)
-            execChildProcessSync(options.imagePath,tilesDir,5,1)
-            execChildProcessSync(options.imagePath,tilesDir,4,1)
-            execChildProcessSync(options.imagePath,tilesDir,3,1)
-            execChildProcessSync(options.imagePath,tilesDir,2,1)
-            execChildProcessSync(options.imagePath,tilesDir,1,1)
-            execChildProcessSync(options.imagePath,tilesDir,11,2)
-            execChildProcessSync(options.imagePath,tilesDir,10,2)
-            execChildProcessSync(options.imagePath,tilesDir,9,2)
-            execChildProcessSync(options.imagePath,tilesDir,8,2)
-            execChildProcessSync(options.imagePath,tilesDir,7,2)
-            execChildProcessSync(options.imagePath,tilesDir,6,2)
-            execChildProcessSync(options.imagePath,tilesDir,5,2)
-            execChildProcessSync(options.imagePath,tilesDir,4,2)
-            execChildProcessSync(options.imagePath,tilesDir,3,2)
-            execChildProcessSync(options.imagePath,tilesDir,2,2)
-
-
-            //do the last conversion, to trigger processing
-            var position=getPosition(1,2);
-            var location="+"+position[0]+"+"+position[1];
-            var childProcess = spawn("convert", [
-                options.imagePath,
-                "-crop",
-                "150x40"+location,
-                tilesDir+"/tile_2_1.jpg"
-
-            ]);
-
-            var childProcessError = undefined;
-
-            childProcess.stdout.on('data', function(data) {
-                update(sessionId, `stdout: ${data}`);
-                console.log(sessionId, `stdout: ${data}`);
-            });
-
-            childProcess.stderr.on('data', function(data) {
-                update(sessionId, `stderr: ${data}`);
-                console.log(sessionId, `stderr: ${data}`);
-            });
-
-            childProcess.on('error', function(err) {
-                update(sessionId, `ERROR: ${err.toString()}`);
-                console.log(sessionId, `ERROR: ${err.toString()}`);
-                childProcessError = err;
-            });
-
-            childProcess.on('close', function(code) {
-                update(sessionId, `child process exited with code ${code}`);
-                //realtime.emit(`child process exited with code ${code}`);
-                console.log(sessionId, `child process exited with code ${code}`);
-                if (code == 0) {
-                    for (var r = 0; r < rows; r++) {
-                        //for (var c=0; c<cols; c++) {
-
-                        if (parseData.tiles[r] == undefined) {
-                            parseData.tiles[r] = [];
-                        }
-
-                        //loop over columns
-                        for (var c = 0; c < cols; c++) {
-
-                            if (parseData.tiles[r][c] == undefined) {
-                                parseData.tiles[r][c] = {};
-                            }
-
-                            var x = c + 1 //* tileWidth;
-                            var y = r + 1//* tileHeight;
-                            var output = tilesDir + "/tile_" + x + "_" + y + ".jpg";
-
-                            parseData.tiles[r][c].path = output;
-                            parseData.tiles[r][c].size = {
-                                width: 150,//Math.min(tileWidth, parseData.imageWidth - x),
-                                height: 40//Math.min(tileHeight, parseData.imageHeight - y)
-                            }
-                            var position=getPosition(y,x)
-                            parseData.tiles[r][c].position = {
-                                x: position[0],
-                                y: position[1]
-                            }
-                        }
-                    }
-                }
-
-                callback(childProcessError, parseData);
-            });
-
-        });
-*/
-
 
 }
-
-
-
-
 
 function processImages(sessionId, imageData, callback) {
     update(sessionId, "performing analysis on images...")
@@ -385,7 +201,7 @@ function processImages(sessionId, imageData, callback) {
     totalAnalysisRequests = 0;
     completeAnalysisRequests = 0;
     var requests = [];
-
+    //TODO: refactor
     //loop over cols
     for (var r = 0; r < imageData.tiles.length; r++) {
 
@@ -399,21 +215,12 @@ function processImages(sessionId, imageData, callback) {
         }
     }
 
-
-
     async.parallelLimit(requests, 8, function() {
         totalAnalysisRequests++;
         callback(imageData);
     })
 
 }
-
-
-
-
-
-
-
 
 
 function analyzeImage(sessionId, _image) {
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -49,24 +49,6 @@ function debug() {
     renderResult('{"imagePath":"./uploads/12377b3c-7561-47ce-8a43-814aec8dabbf/image.jpg","jsonPath":"./uploads/12377b3c-7561-47ce-8a43-814aec8dabbf/image.json"}')
 }
 
-/*
-$(document).ready(function() {
-    $("#sessionId").val(id);
-
-    scaleSlider = $("#scaleSlider").bootstrapSlider();
-    scaleSlider.bootstrapSlider()
-    scaleSlider.on("change", function(event) {
-        setContentScale(event.value.newValue);
-    })
-
-    $("#overlayToggle").change(function(event) {
-        var checked = $(this).prop('checked')
-
-        $("#render table").css('visibility', checked ? 'visible' : 'hidden');
-    });
-
-})
-*/
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
@@ -107,19 +89,11 @@ function resetView() {
     $("#feedback").html("");
 }
 
-function setContentScale(targetScale) {
-
-    $("#render").css("transform", "scale(" + targetScale + ")")
-    scaleSlider.bootstrapSlider('setValue', targetScale);
-}
-
 function renderResult(dataStr) {
     var data = JSON.parse(dataStr)
     console.log(data)
 
     var renderContainer = $("#render");
-
-    //renderContainer.append($("<img class='' src='" + data.imagePath + "'  />"));
 
     $.ajax({
             type: 'GET',
@@ -139,10 +113,6 @@ function renderResult(dataStr) {
             $("#footerControls").removeClass("hidden");
             $("#content").addClass("hidden");
 
-/*
-            var targetScale = $("#render-parent").width() / result.imageWidth
-            setContentScale(targetScale);
-            */
         })
         .fail(function(jqXHR, status) {
             console.log("Request failed: " + status);
@@ -154,7 +124,7 @@ function constructDiv(data,imagePath) {
     console.log(JSON.stringify(data, null, 2));
 //    <div style="position: relative; background: url(path to image); width: (width)px; height: (height)px;">
     var div = $("<div>");
-
+    
     //check error
     var error = data.tiles[0][0].analysis.error;
     if (error !== undefined)
@@ -168,17 +138,8 @@ function constructDiv(data,imagePath) {
         return div;
     }
     
-    //div.css("position", "relative");
-//    div.css("background", imagePath);
-    //table.append($("<tr>"));
-    //table.append($("<td>"));
-    //table.append($("<img class='' src='" + imagePath + "' width='50%' />"));
-    //table.append($("</td></tr>"));
-   
-    //var faceDetectionData = $("<div>");
     console.log("");
-    
-    
+        
     console.log(JSON.stringify(data.tiles[0][0].analysis.images, null, 2));
     //assume one image
     image = data.tiles[0][0].analysis.images[0]
@@ -192,8 +153,7 @@ function constructDiv(data,imagePath) {
     var row = $("<tr>");
     var cell = $("<td>");
     var imageDiv = $('<div class="imageDiv">');
-    //cell.append($("<img class='' src='" + imagePath + "' width='50%' />"));
-    imageDiv.append($("<img class='' src='" + imagePath + "' />"));
+    imageDiv.append($("<img class='' src='" + imagePath + "' width='640px' />"));
     for (var i=0 ;i<facesLength ; i++)
     {
         var facedata=image.faces[i];
@@ -219,8 +179,7 @@ function constructDiv(data,imagePath) {
     cell.append(imageDiv);
     row.append(cell);
     table.append(row);  
-    //table.append($("<tr>"));
-    //table.append($("<td>"));
+
     if (facesLength==0)
     {
         row = $("<tr>");
@@ -253,14 +212,7 @@ function constructDiv(data,imagePath) {
             table.append(row);  
         }
     }
-    //table.append($("</td>"));
-    //table.append($("</tr>"));
-    //        console.log(JSON.stringify(face, null, 2));
-        //div.append(face.age.min);
-        //div.append("<br/>");
-        //div.append(face.age.max);
     console.log("");
-    //div.append(faceDetectionData);
 
     //classification data
     console.log(JSON.stringify(data.tiles[0][0].analysis_classify.images, null, 2));
@@ -268,8 +220,6 @@ function constructDiv(data,imagePath) {
     image = data.tiles[0][0].analysis_classify.images[0];
     var classifiers=image.classifiers;
     var classifiersLength=image.classifiers.length;
-    //table.append($("<tr>"));
-    //table.append($("<td>"));
     
     for (var i=0 ;i<classifiersLength ; i++)
     {
@@ -286,121 +236,8 @@ function constructDiv(data,imagePath) {
             table.append(row);  
                     
         }
-//        console.log("classes");
-  //      console.log(classes);
     }
 
-    /*//    div.css("width", data.imageWidth);
-//    div.css("height", data.imageHeight);
-
-
-    var table = $("<table>");
-    table.css("width", data.imageWidth);
-    table.css("height", data.imageHeight);
-    var row = $("<tr>");
-    var tableData = $("<td>");
-
-
-
-    var rows = data.tiles
-    for (var r = 0; r < rows.length; r++) {
-        var cols = data.tiles[r]
-        //var row = $("<div>");
-
-        for (var c = 0; c < cols.length; c++) {
-            var cell = $("<div>");
-            var cellData = cols[c];
-            cell.css("position", "absolute");
-            cell.css("width", cellData.size.width);
-            cell.css("height", cellData.size.height);
-            cell.css("top", cellData.position.y);
-            cell.css("left", cellData.position.x);
-
-            var style = getAnalysis(cellData);
-
-            cell.css("background", style)
-
-            cell.html(getConfidence(cellData));
-            //div.append(cell);
-            tableData.append(cell);
-        }
-
-        //div.append(row);
-    }
-
-    //tableData.append(div)
-    row.append(tableData)
-    table.append(row)
-*/
     div.append(table);
     return div;
-}
-
-
-function constructTable_original(data) {
-    var table = $("<table>");
-    table.css("width", data.imageWidth);
-    table.css("height", data.imageHeight);
-
-    var rows = data.tiles
-    for (var r = 0; r < rows.length; r++) {
-        var cols = data.tiles[r]
-        var row = $("<tr>");
-
-        for (var c = 0; c < cols.length; c++) {
-            var cell = $("<td>");
-            var cellData = cols[c];
-            cell.css("width", cellData.size.width);
-            cell.css("height", cellData.size.height);
-
-            var style = getAnalysis(cellData);
-
-            cell.css("background", style)
-
-            cell.html(getConfidence(cellData));
-            row.append(cell);
-        }
-
-        table.append(row);
-    }
-
-    return table;
-}
-
-function getAnalysis(cellData) {
-    if (cellData.analysis && cellData.analysis.images && cellData.analysis.images.length > 0) {
-        var image = cellData.analysis.images[0];
-        if (image && image.classifiers && image.classifiers.length > 0) {
-            var classifier = cellData.analysis.images[0].classifiers[0]
-
-            if (classifier && classifier.classes && classifier.classes.length > 0) {
-
-                // this demo only visualizes the first classification within the first classifier
-                // however could be modified to support multiple classifiers
-
-                var classification = classifier.classes[0];
-                return "rgba(0,255,0," + Math.min(classification.score * 2, 0.9) + ")"
-            }
-        }
-    }
-    return "rgba(0,0,0,0)"
-}
-
-function getConfidence(cellData) {
-    if (cellData.analysis && cellData.analysis.images && cellData.analysis.images.length > 0) {
-        var image = cellData.analysis.images[0];
-        if (image && image.classifiers && image.classifiers.length > 0) {
-            var classifier = cellData.analysis.images[0].classifiers[0]
-
-            if (classifier && classifier.classes && classifier.classes.length > 0) {
-
-                // this demo only visualizes the first classification within the first classifier
-                // however could be modified to support multiple classifiers
-
-                var classification = classifier.classes[0];
-                return classification.score.toFixed(3)
-            }
-        }
-    }
-    return "-"
 }
